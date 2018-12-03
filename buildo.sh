6e27268a3b426a4ae79ba9154fafcca0f15fde36
Black-J88 <nitin88mehta@gmail.com>
2018-12-03 14:14:37 +0530

    Added a small(Noob) script to automate compilation

---
+++ buildo.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Cleaning previous out directory before building"
+rm -rf out
+echo "Creating new out directory"
+mkdir out
+echo "Writing configurations"
+make O=out ARCH=arm64 lineage_A6020_defconfig
+echo "Building Image"
+make  O=out ARCH=arm64 CROSS_COMPILE=/home/black_j/BJ/toolchain_aarch64/aarch64-linaro-linux-android/bin/aarch64-linaro-linux-android-
